20bd2cf802faec2a9a971ae81e8cea357de8bcbf
root <root@DESKTOP-L611DB1.localdomain>
2021-04-18 13:10:57 +1000

    execute method is now using a session from the bind

--- back-end/src/controllers/test_controller.py
+++ back-end/src/controllers/test_controller.py
@@ -1,11 +1,10 @@
 import flask
+from main import db
 from models.Test import Test
 from validation_schemas.TestSchema import TestSchema
 
 tests = flask.Blueprint('tests', __name__)
 
-import psycopg2
-from psycopg2.extras import RealDictCursor
 import os
 import json
 import datetime
@@ -21,34 +20,37 @@ def detail_get_test(test_id):
 
 @tests.route('/test/<test_id>/execute', methods=["POST"])
 def execute_query_on_db(test_id):
+    # session creation
+    binding = db.get_engine(bind="secondary_schema")
+    sess = db.create_scoped_session(options = {'bind': binding})
+    # use secondary_schema
+    sess.execute("SET search_path TO secondary_schema;")
+
     # retrieves the query_as_answer to run alongside user submitted
     question_answer = Test.query.get(test_id).question.answer_as_query
     # initialize an output dict
-    result = {} 
+    result = {}
+    answer = {} 
 
-    conn = psycopg2.connect(
-        host=os.getenv("HOST"), 
-        dbname=flask.current_app.config["SQLALCHEMY_DATABASE_URI"].split("/")[-1],
-        user="interactor", password=os.getenv("PASSWORD"),
-        port=os.getenv("PORT")
-    )
-
-    # returns the result set
-    with conn.cursor(cursor_factory=RealDictCursor) as curs:
-        curs.execute("""SET search_path TO secondary_schema;""")
-        curs.execute(flask.request.json["query"])
-        result["result_set"] = curs.fetchall()
+    result_curs = sess.execute(flask.request.json["query"])
+    result["keys"] = result_curs.keys()._keys
+    result["rows"] = [[c for c in r] for r in result_curs.fetchall()]
     
-    # runs the query_as_answer and compares results with user submitted
-    with conn.cursor(cursor_factory=RealDictCursor) as curs:
-        curs.execute("""SET search_path TO secondary_schema;""")
-        curs.execute(question_answer)
-        answer_result = curs.fetchall()
-        
-        if json.dumps(result["result_set"], default=myconverter) == json.dumps(answer_result, default=myconverter):
-            result["matches"] = True # if they match, matches is set to true
-        else:
-            result["matches"] = False
+    
+    # # runs the query_as_answer and compares results with user submitted
+    
+    answer_result = sess.execute(question_answer)
+    
+    answer["keys"] = answer_result.keys()._keys
+    answer["rows"] = [[c for c in r] for r in answer_result.fetchall()]
+    print(answer)
+
+    if flask.jsonify(result["rows"]).get_json() == flask.jsonify(answer["rows"]).get_json():
+        result["matches"] = True # if they match, matches is set to true
+        result["message"] = "correct"
+    else:
+        result["matches"] = False
+        result["message"] = f"was expecting {len(answer['rows'])} rows and {len(answer['keys'])} columns"
 
     return flask.jsonify(result)
 
